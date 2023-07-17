6f7bd15f6104f6f383377f24298cd45378742dfb
AncyDavid1 <139550259+AncyDavid1@users.noreply.github.com>
2023-07-17 16:08:26 +0530

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -1,13 +1,7 @@
-def count_word_frequency(sentence):
-    words = sentence.lower().split()
-    frequency = {}
-    for word in words:
-        contained_word = frequency.get(word, 0) + 1
-        frequency[word]=contained_word
-
-    return frequency
-sentence = input("Enter some words: ")
-word_frequency = count_word_frequency(sentence)
-
-for word, count in word_frequency.items():
-    print(word.capitalize(), "-", count)
+sentence=input("Enter the string").lower()
+x =sentence.split(" ")
+y=[]
+for i in x:
+ if y.count(i)==0:
+  y.append(i)
+  print(i.title(),"-",sentence.count(i))
